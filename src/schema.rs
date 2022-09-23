d9b82fac06068efa52ecccb9d267c068873e4d87
Stanley Winata <winatastanley355@gmail.com>
2022-09-23 15:54:46 +0700

    Add initial migration

---
+++ src/schema.rs
@@ -0,0 +1,11 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Uuid,
+        fullname -> Varchar,
+        email -> Varchar,
+        password -> Nullable<Varchar>,
+        phone_number -> Nullable<Varchar>,
+    }
+}
