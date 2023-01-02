21780de6db73dada75775941e4396e1f3222748b
Stanley Winata <winatastanley355@gmail.com>
2023-01-02 16:05:39 +0700

    refac unit test

--- src/user/tests/mod.rs
+++ src/user/tests/mod.rs
@@ -1,10 +1,12 @@
-use super::model::User;
-use actix_web::web::Data;
-
 #[test]
 fn user_checks() {
     let db_url = "postgres://postgres:password@localhost/author".to_string();
     let pool = crate::db::build_pool(&db_url);
-    let user_check = User::check_user(Data::new(pool.unwrap()), "stanley.winata@lifepal.co.id".to_string());
-    assert_eq!(user_check.unwrap().email, "stanley.winata@lifepal.co.id".to_string());
+    let default_email = "stanley.winata@lifepal.co.id".to_string();
+    let user_check = super::model::User::check_user(actix_web::web::Data::new(pool.unwrap()), default_email.clone());
+
+    match user_check {
+        Ok(user) => assert_eq!(user.email, default_email),
+        Err(_) => assert_eq!("DB connection failure", default_email)
+    }
 }
