98afcfd9f32f0a8f8b62011ddc6fa88521f16f20
Stanley Winata <winatastanley355@gmail.com>
2023-05-01 16:05:57 +0700

    The first file, handler.rs, has some changes to the use statements and the function definition for gmail_login. The second file, email.rs, has a change to the function is_valid_email.
    In the first file, handler.rs, the use statements have been changed to include a new crate, actix_web. The function definition for gmail_login has also been changed to take an additional parameter, body, which is of type web::Json<GmailLoginReq>.
    
    In the second file, email.rs, the function is_valid_email has been changed to be a public function.

--- src/user/handler.rs
+++ src/user/handler.rs
@@ -1,15 +1,9 @@
 use super::req::GmailLoginReq;
-use  super::res::ErrorRes;
 use crate::db::PgPool;
 use actix_web::{post, web, HttpResponse};
 
 #[post("/login/gmail/")]
 async fn gmail_login(pool: web::Data<PgPool>, body: web::Json<GmailLoginReq>) -> HttpResponse {
-    println!("{:?} test: ", body);
-    HttpResponse::Accepted().json(ErrorRes {
-        error: "Error".to_string(),
-        message: "salah woi".to_string()
-    })
 }
 
 pub fn route(config: &mut web::ServiceConfig) {
