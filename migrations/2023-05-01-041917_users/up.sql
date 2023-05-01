517794733b549967ae977c88d52eb335b32c7901
Stanley Winata <winatastanley355@gmail.com>
2023-05-01 11:21:11 +0700

    User migration

---
+++ migrations/2023-05-01-041917_users/up.sql
@@ -0,0 +1,9 @@
+-- Your SQL goes here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  fullname VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  password VARCHAR NOT NULL,
+  phone_number VARCHAR
+);
--- src/schema.rs
+++ src/schema.rs
@@ -1,1 +1,11 @@
+// @generated automatically by Diesel CLI.
 
+diesel::table! {
+    users (id) {
+        id -> Uuid,
+        fullname -> Varchar,
+        email -> Varchar,
+        password -> Varchar,
+        phone_number -> Nullable<Varchar>,
+    }
+}
