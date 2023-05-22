d5d57cb5cc3de74f7a94ca4ffe82d4a5375c8d41
stan <winatastanley355@gmail.com>
2023-05-22 10:52:48 +0700

    Checkbot migration

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,18 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    checkbots (id) {
+        id -> Int4,
+        user_id -> Uuid,
+        created_at -> Timestamp,
+        source_text_token -> Int4,
+        checkbot_text_token -> Int4,
+        source_text -> Varchar,
+        checkbot_text -> Varchar,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Uuid,
         fullname -> Varchar,
@@ -9,3 +21,10 @@ diesel::table! {
         phone_number -> Nullable<Varchar>,
     }
 }
+
+diesel::joinable!(checkbots -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    checkbots,
+    users,
+);
