b28020fff278a87933a7a0bb6551cea9f25673f1
stan <winatastanley355@gmail.com>
2023-05-22 10:40:07 +0700

    Checkbot model

---
+++ src/checkbot/model.rs
@@ -0,0 +1,14 @@
+use actix_web::web;
+use diesel::types::Timestamp;
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Debug, Clone, Deserialize, Serialize)]
+pub struct Checkbot {
+  pub id: u32,
+  pub user_id: uuid::Uuid,
+  pub created_at: Timestamp,
+  pub source_text_token: u32,
+  pub checkbot_text_token: u32,
+  pub source_text: String,
+  pub checkbot_text: String,
+}
