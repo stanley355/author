7be892cec346cb62f5afefc52a15016427874456
stan <winatastanley355@gmail.com>
2023-05-22 11:43:34 +0700

    Checkbot model and handler

--- src/checkbot/handler.rs
+++ src/checkbot/handler.rs
@@ -1,12 +1,15 @@
 use crate::db::PgPool;
+use super::model::Checkbot;
 use super::req::NewCheckbotReq;
 use actix_web::{post, web, HttpResponse};
 
 #[post("/")]
-async fn new(pool: web::Data<PgPool>, body: web::Json<NewCheckbotReq>) -> HttpResponse {
-  HttpResponse::Ok().body(format!("hi"))
+async fn add_checkbot(pool: web::Data<PgPool>, body: web::Json<NewCheckbotReq>) -> HttpResponse {
+  let b = Checkbot::new(&pool, body);
+  println!("ihihi");
+  HttpResponse::InternalServerError().body(format!("hi"))
 }
 
 pub fn route(config: &mut web::ServiceConfig) {
-    config.service(new);
+    config.service(add_checkbot);
 }
--- src/checkbot/model.rs
+++ src/checkbot/model.rs
@@ -1,15 +1,35 @@
 use actix_web::web;
-use diesel::Queryable;
-use diesel::sql_types::Timestamp;
+use diesel::{Queryable, QueryResult, ExpressionMethods, RunQueryDsl, sql_types::Integer};
 use serde::{Deserialize, Serialize};
+use crate::{db::PgPool, schema::checkbots};
+use super::req::NewCheckbotReq;
 
 #[derive(Queryable, Debug, Clone, Deserialize, Serialize)]
 pub struct Checkbot {
-  pub id: u32,
+  pub id: i32,
   pub user_id: uuid::Uuid,
   pub created_at: chrono::NaiveDateTime,
-  pub source_text_token: u32,
-  pub checkbot_text_token: u32,
+  pub source_text_token: i32,
+  pub checkbot_text_token: i32,
   pub source_text: String,
   pub checkbot_text: String,
 }
+
+impl Checkbot {
+  pub fn new(pool: &web::Data<PgPool>, body: web::Json<NewCheckbotReq>) -> QueryResult<Checkbot> {
+    let conn = pool.get().unwrap();
+    let uuid = uuid::Uuid::parse_str(&body.user_id).unwrap();
+    let data = (
+        (checkbots::user_id.eq(uuid)),
+        (checkbots::source_text_token.eq(1)),
+        (checkbots::checkbot_text_token.eq(1)),
+        (checkbots::source_text.eq(&body.source_text)),
+        (checkbots::source_text.eq(&body.checkbot_text))
+    );
+
+    diesel::insert_into(checkbots::table)
+        .values(data)
+        .get_result(&conn)
+  }
+}
+
