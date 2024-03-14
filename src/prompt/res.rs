7fc3e6d2bd0a3805e3cf97c2d0f3216b4d9900ea
Stanley Winata <winatastanley355@gmail.com>
2024-03-14 11:32:40 +0700

    Fix response

--- src/prompt/res.rs
+++ src/prompt/res.rs
@@ -1,3 +1,4 @@
+use actix_web::http::StatusCode;
 use serde::{Deserialize, Serialize};
 
 use super::req::NewPromptReq;
@@ -5,8 +6,8 @@ use super::req::NewPromptReq;
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct NewPromptRes {
     pub user_id: String,
-    pub system_prompt: String,
-    pub user_prompt: String,
+    pub status: u16,
+    pub status_message: String,
     pub completion_text: String,
 }
 
@@ -14,8 +15,8 @@ impl NewPromptRes {
     pub fn new(req: NewPromptReq, completion_text: String) -> Self {
         NewPromptRes {
             user_id: req.user_id,
-            system_prompt: req.system_prompt,
-            user_prompt: req.user_prompt,
+            status: StatusCode::OK.as_u16(),
+            status_message: "OK".to_string(),
             completion_text,
         }
     }
