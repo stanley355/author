2febbc6da988e5827961e62d7f6f84e08dec8938
Stanley Winata <winatastanley355@gmail.com>
2024-03-18 10:54:00 +0700

    Update paid topup

--- src/topup/handler.rs
+++ src/topup/handler.rs
@@ -1,8 +1,8 @@
 use super::model::TopUp;
 use super::req::TopupPaidReq;
-use crate::db::PgPool;
 use crate::topup::req::TopupPayasyougoReq;
 use crate::util::web_response::WebErrorResponse;
+use crate::{db::PgPool, user::model::User};
 
 use actix_web::{post, web, HttpResponse};
 
@@ -28,9 +28,25 @@ async fn new_paid_topup(pool: web::Data<PgPool>, body: web::Json<TopupPaidReq>) 
 
     match result {
         Ok(topup) => {
-            
+            if topup.topup_type == "topup".to_string() {
+                let update_user_balance =
+                    User::increase_balance(&pool, topup.user_id, topup.topup_amount);
+
+                match update_user_balance {
+                    Ok(_) => {
+                        return HttpResponse::Ok().json(topup);
+                    }
+                    Err(err) => {
+                        let err_res = WebErrorResponse::server_error(
+                            err,
+                            "Fail to update balance, please try again",
+                        );
+                        return HttpResponse::InternalServerError().json(err_res);
+                    }
+                }
+            }
             HttpResponse::Ok().json(topup)
-        },
+        }
         Err(err) => {
             let err_res = WebErrorResponse::server_error(err, "Fail to update, please try again");
             HttpResponse::InternalServerError().json(err_res)
