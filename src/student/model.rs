766c2013ef63a113b8a107ee251e02d8870972f2
Stanley Winata <winatastanley355@gmail.com>
2024-04-04 14:36:12 +0700

    Insert student to db

--- src/student/model.rs
+++ src/student/model.rs
@@ -1,7 +1,14 @@
-use chrono::NaiveDateTime;
+use super::req::NewStudentReq;
+use crate::diesel::ExpressionMethods;
+use crate::{db::PgPool, schema::students};
+
+use actix_web::web;
+use chrono::{Duration, NaiveDateTime, Utc};
+use diesel::{QueryResult, RunQueryDsl};
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Clone)]
+#[derive(Queryable, Debug, Clone, Deserialize, Serialize)]
 pub struct Student {
     id: Uuid,
     user_id: Uuid,
@@ -16,4 +23,45 @@ pub struct Student {
     half_discount_end_at: NaiveDateTime,
     student_application_valid: bool,
     student_application_invalid_reason: Option<String>,
+}
+
+impl Student {
+    fn calc_discount_end_at(is_free_discount: bool) -> NaiveDateTime {
+        let days = match is_free_discount {
+            true => 366,
+            false => 732,
+        };
+
+        let current_time = Utc::now();
+        let end_time = current_time
+            .checked_add_signed(Duration::days(days))
+            .unwrap();
+        return end_time.naive_utc();
+    }
+
+    pub fn insert_one(
+        pool: web::Data<PgPool>,
+        new_student_req: NewStudentReq,
+    ) -> QueryResult<Student> {
+        let user_id = Uuid::parse_str(&new_student_req.user_id).unwrap();
+        let insti_level = &new_student_req.institution_level.to_string();
+        let free_disc_end_at = Self::calc_discount_end_at(true);
+        let half_disc_end_at = Self::calc_discount_end_at(false);
+
+        let data = (
+            (students::user_id.eq(user_id)),
+            (students::student_id.eq(&new_student_req.student_id)),
+            (students::student_email.eq(&new_student_req.student_email)),
+            (students::student_card_img_url.eq(&new_student_req.student_card_img_url)),
+            (students::institution_level.eq(insti_level)),
+            (students::institution_name.eq(&new_student_req.institution_name)),
+            (students::free_discount_end_at.eq(free_disc_end_at)),
+            (students::half_discount_end_at.eq(half_disc_end_at)),
+        );
+
+        let conn = pool.get().unwrap();
+        diesel::insert_into(students::table)
+            .values(data)
+            .get_result::<Student>(&conn)
+    }
 }
--- src/student/req.rs
+++ src/student/req.rs
@@ -5,10 +5,10 @@ use super::enums;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct NewStudentReq {
-    user_id: String,
-    student_id: String,
-    student_email: Option<String>,
-    student_card_img_url: String,
-    institution_level: enums::InstitutionLevel,
-    institution_name: String,
+    pub user_id: String,
+    pub student_id: String,
+    pub student_email: Option<String>,
+    pub student_card_img_url: String,
+    pub institution_level: enums::InstitutionLevel,
+    pub institution_name: String,
 }
