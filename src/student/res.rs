8fe0167895a04e6fc3fbad6fb7514d60573a947e
Stanley Winata <winatastanley355@gmail.com>
2024-04-05 13:26:23 +0700

    Fix active stuent discount on account

--- src/student/res.rs
+++ src/student/res.rs
@@ -6,3 +6,9 @@ pub struct StudentDiscountAvailabilityRes {
   pub is_free_discount: bool,
   pub is_half_discount: bool
 }
+
+impl StudentDiscountAvailabilityRes {
+    pub fn to_tuple(self) -> (bool, bool, bool) {
+      (self.is_student, self.is_free_discount, self.is_half_discount)
+    }
+}
