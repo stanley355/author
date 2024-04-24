b57a08991aed043e87c46686b6fe90b804d7c46c
Stanley Winata <winatastanley355@gmail.com>
2024-04-24 17:20:24 +0700

    Middleware

--- src/v2/middleware/author_middleware_service.rs
+++ src/v2/middleware/author_middleware_service.rs
@@ -1,11 +1,38 @@
-use actix_web::dev::{forward_ready, Service, ServiceRequest, ServiceResponse};
-use std::{future, pin::Pin};
+use actix_web::{
+    dev::{forward_ready, Service, ServiceRequest, ServiceResponse},
+    error::ErrorUnauthorized,
+};
+use std::{env, future, pin::Pin};
 
 // type LocalBoxFuture<T> = Pin<Box<dyn Future<Output = T> + 'static>>;
 type PinBoxFuture<R, E> = Pin<Box<dyn future::Future<Output = Result<R, E>>>>;
 
+#[derive(Clone, Copy)]
 pub struct AuthorMiddlewareService<S> {
     pub service: S,
+}
+
+impl<S, B> AuthorMiddlewareService<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = actix_web::Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    fn respond_ok(
+        &self,
+        req: ServiceRequest,
+    ) -> PinBoxFuture<ServiceResponse<B>, actix_web::Error> {
+        let service_future = self.service.call(req);
+
+        Box::pin(async move {
+            let service_response = service_future.await?;
+            Ok(service_response)
+        })
+    }
+
+    fn respond_err(&self) -> PinBoxFuture<ServiceResponse<B>, actix_web::Error> {
+        Box::pin(async { Err(ErrorUnauthorized("".to_string())) })
+    }
 }
 
 impl<S, B> Service<ServiceRequest> for AuthorMiddlewareService<S>
@@ -22,11 +49,20 @@ where
     forward_ready!(service);
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
-        let service_future = self.service.call(req);
+        if req.path() == "/v1/topups/doku/notification/" || req.path().contains("/v1/files") {
+            return self.respond_ok(req);
+        }
 
-        Box::pin(async move {
-            let service_response = service_future.await?;
-            Ok(service_response)
-        })
+        if let Some(authorization) = req.headers().get("authorization") {
+            let token = authorization.to_str().unwrap();
+            let bearer_token = env::var("BEARER_TOKEN").expect("BEARER_TOKEN must be set");
+
+            return match token == bearer_token {
+                true => self.respond_ok(req),
+                false => self.respond_err(),
+            };
+        }
+
+        return self.respond_err();
     }
 }
