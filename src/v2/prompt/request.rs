22d75330269439fac5a25e10bfc099434308fbc1
Stanley Winata <winatastanley355@gmail.com>
2024-04-25 09:31:03 +0700

    Prompt v2 request

---
+++ src/v2/prompt/request.rs
@@ -0,0 +1,21 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum PromptType {
+  Chat
+}
+
+impl fmt::Display for PromptType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct NewPromptRequestBody {
+    pub user_id: String,
+    pub prompt_type: PromptType,
+    pub system_prompt: String,
+    pub user_prompt: String,
+}
