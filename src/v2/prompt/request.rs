3a11db3eeb9e7180a702e02a46500c975ca7f869
Stanley Winata <winatastanley355@gmail.com>
2024-05-06 16:18:42 +0700

    Update image to text v2 function

--- src/v2/prompt/request.rs
+++ src/v2/prompt/request.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub enum PromptType {
     Translate,
     GrammarCheck,
@@ -14,10 +14,17 @@ impl fmt::Display for PromptType {
     }
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct NewPromptRequestBody {
     pub user_id: String,
     pub prompt_type: PromptType,
     pub system_content: String,
     pub user_content: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpdateImageToTextRequestBody {
+    pub user_id: String,
+    pub prompt_id: i32,
+    pub completion_text: String
 }
