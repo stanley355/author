f61aa0a7e08dd9a1fe72045b29fc2846242ed486
Stanley Winata <winatastanley355@gmail.com>
2024-07-25 12:52:52 +0700

    Middleware

--- migrations/2024-07-25-031038_subscriptions/down.sql
+++ migrations/2024-07-25-031038_subscriptions/down.sql
@@ -2,5 +2,5 @@
 ALTER TABLE subscriptions ADD COLUMN topup_id uuid REFERENCES topups(id);
 ALTER TABLE subscriptions ADD COLUMN is_paylater boolean DEFAULT false;
 ALTER TABLE subscriptions DROP COLUMN price;
-ALTER TABLE students ADD COLUMN student_application_valid BOOLEAN NOT NULL DEFAULT true;
-ALTER TABLE students ADD COLUMN student_application_invalid_reason VARCHAR;
+-- ALTER TABLE students ADD COLUMN student_application_valid BOOLEAN NOT NULL DEFAULT true;
+-- ALTER TABLE students ADD COLUMN student_application_invalid_reason VARCHAR;
--- migrations/2024-07-25-031038_subscriptions/up.sql
+++ migrations/2024-07-25-031038_subscriptions/up.sql
@@ -2,5 +2,5 @@
 ALTER TABLE subscriptions DROP COLUMN topup_id;
 ALTER TABLE subscriptions DROP COLUMN is_paylater;
 ALTER TABLE subscriptions ADD COLUMN price FLOAT NOT NULL DEFAULT 0.0;
-ALTER TABLE students DROP COLUMN student_application_valid;
-ALTER TABLE students DROP COLUMN student_application_invalid_reason;
+-- ALTER TABLE students DROP COLUMN student_application_valid;
+-- ALTER TABLE students DROP COLUMN student_application_invalid_reason;
