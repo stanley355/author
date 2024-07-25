b271a4163ec80d9fbc6af97e0a3005ed613ec83e
Stanley Winata <winatastanley355@gmail.com>
2024-07-25 17:54:40 +0700

    Http error

--- src/prompts/services.rs
+++ src/prompts/services.rs
@@ -1,6 +1,9 @@
-use actix_web::{get, post, web, HttpResponse};
+use super::model::Prompt;
+use super::payment::PromptPayment;
 use super::request::NewPromptRequest;
+use super::request::PromptType;
 use crate::{db::PgPool, http_error::HttpError};
+use actix_web::{get, post, web, HttpResponse};
 
 #[post("/")]
 async fn post_prompt(
@@ -9,7 +12,20 @@ async fn post_prompt(
 ) -> HttpResponse {
     let request = request_json.into_inner();
 
-    HttpResponse::Ok().body("woi")
+    return match &request.prompt_type {
+        PromptType::Translate | PromptType::Checkbot | PromptType::PhoneticTranscriptions => {
+            let user_id = uuid::Uuid::parse_str(&request.user_id).unwrap();
+            let prompt_payment = Prompt::check_payment(&user_id, &request.prompt_type);
+
+            match prompt_payment {
+                PromptPayment::PaymentRequired => HttpError::payment_required(),
+                _ => HttpResponse::Ok().body("woi"),
+            }
+        }
+        _ => HttpError::bad_request(
+            "Only Translate, Checkbot, and PhoneticTranscriptions prompt_type accepted",
+        ),
+    };
 }
 
 pub fn services(config: &mut web::ServiceConfig) {
