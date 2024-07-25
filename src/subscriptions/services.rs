787f54ac1ec599c5f999baf96e5962bc7e5e97fd
Stanley Winata <winatastanley355@gmail.com>
2024-07-25 11:39:39 +0700

    Middleware

--- src/subscriptions/services.rs
+++ src/subscriptions/services.rs
@@ -1,7 +1,7 @@
 use actix_web::{post, web, HttpResponse};
 
 use super::model::Subscription;
-use super::request::NewSubscriptionRequest;
+use super::request::{NewSubscriptionRequest, DokuNotificationRequest};
 use crate::{db::PgPool, http_error::HttpError};
 
 #[post("/")]
@@ -15,6 +15,20 @@ async fn post_subscription(
         Ok(subscription) => HttpResponse::Created().json(subscription),
         Err(diesel_error) => HttpError::internal_server_error(&diesel_error.to_string()),
     }
+}
+
+#[post("/doku/notification/")]
+async fn post_doku_notificatioin(
+    pool: web::Data<PgPool>,
+    json_request: web::Json<DokuNotificationRequest>,
+) -> HttpResponse {
+    let request = json_request.into_inner();
+    // let subscription_result = Subscription::new_insert(&pool, &request, false);
+    // match subscription_result {
+    //     Ok(subscription) => HttpResponse::Created().json(subscription),
+    //     Err(diesel_error) => HttpError::internal_server_error(&diesel_error.to_string()),
+    // }
+    HttpResponse::Ok().body("paid")
 }
 
 pub fn services(config: &mut web::ServiceConfig) {
